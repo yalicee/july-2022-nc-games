661c40c9d3519b27e525bf07f204c6cf6b60ebc5
Alex Yang <alexyang@Alexs-MacBook-Pro.local>
2022-09-26 13:39:48 +0100

    getReviews

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./components/Home";
+import Reviews from "./components/Reviews"
+
+function App() {
+  return <div className="App">
+    <Home />
+    <Reviews/>
+  </div>;
+}
+
+export default App;
